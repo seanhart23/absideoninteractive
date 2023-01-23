d782fba24d7f3ab47e40997f2fd31370b5737b67
Sean Hart <seanhart23@gmail.com>
2023-01-22 22:06:51 -0500

    Updates for responsive

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,12 @@ router.post('/', function(req, res){
     var newQuoteRequest = {
         fname: req.body.fname,
         lname: req.body.lname,
-        email: req.body.email
+        email: req.body.email,
+        phone: req.body.phone,
+        company: req.body.company,
+        type: req.body.type,
+        description: req.body.description,
+        bodget: req.body.budget
     }
 
     const smtpTrans = nodemailer.createTransport({
@@ -27,9 +32,9 @@ router.post('/', function(req, res){
     })
 
     const mailOpts = {
-        to: 'absideoninteractive@gmail.com',
+        to: 'hello@absideoninteractive.com',
         subject: 'Quote Request from Absideon Interactive',
-        html: '<b>Name:</b> ' + req.body.fname + " " + req.body.lname + "<div><b>Email:</b> " + req.body.email
+        html: '<b>Name:</b> ' + req.body.fname + " " + req.body.lname + "<div><b>Email:</b> " + req.body.email + "</div><div><b>Phone:</b> " + req.body.phone + "</div><div><b>Company:</b> " + req.body.company + "</div><div><b>Project Type:</b> " + req.body.type + "</div><div><b>Budget:</b> " + req.body.budget + "</div><div><b>Project Description:</b> " + req.body.description + "</div>"
     }
 
     smtpTrans.sendMail(mailOpts, (error, response) => {
